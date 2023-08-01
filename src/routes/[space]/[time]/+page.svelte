7f04ecf5cd4b9ea468134f3306e635ea95b3d929
aiacuone <age.iacuone@aventus.io>
2023-08-02 00:00:23 +0100

    replicated top note with bottom notes

--- src/routes/[space]/[time]/+page.svelte
+++ src/routes/[space]/[time]/+page.svelte
@@ -11,33 +11,33 @@
 		<p class="capitalize">: {data.time}</p>
 	</div>
 	<div class="w-full stack gap-6">
-		<div class="stack gap-2">
-			<div class="hStack">
+		<div class="hStack">
+			<div class="w-10/12 bg-white p-2 stack gap-1 rounded-md">
 				<input
 					type="text"
 					placeholder="Subject"
-					class="outline-0 bg-white text-black px-2 rounded-md w-4/12 h-[30px]"
+					class="outline-0 text-black text-opacity-30 w-4/12 h-[30px]"
 				/>
-				<div class="w-full justify-end hStack gap-2">
-					<button class="bg-white bg-opacity-30 rounded-sm p-1"
-						><Icon icon="system-uicons:reset" height="25px" /></button
-					>
-					<button class="bg-white bg-opacity-30 rounded-sm p-1"
-						><Icon icon="mdi:tick" height="25px" /></button
-					>
-				</div>
+				<div
+					class="w-full outline-0 text-black input min-h-[30px] flex items-center break-normal"
+					role="textbox"
+					contenteditable
+					on:input={(e) => console.log(e.target.innerText)}
+				/>
 			</div>
-			<div
-				class="w-full outline-0 bg-white text-black px-2 rounded-md input h-[30px] flex items-center"
-				role="textbox"
-				contenteditable
-				on:input={(e) => console.log(e.target.innerText)}
-			/>
+			<div class="hStack gap-2 center w-2/12">
+				<button class="bg-white bg-opacity-30 rounded-sm p-1"
+					><Icon icon="system-uicons:reset" height="25px" /></button
+				>
+				<button class="bg-white bg-opacity-30 rounded-sm p-1"
+					><Icon icon="mdi:tick" height="25px" /></button
+				>
+			</div>
 		</div>
 		<hr />
 		<div class="hStack">
 			<div class="w-10/12 bg-white p-2 stack gap-2 rounded-md">
-				<p class="text-black text-opacity-20">Test subject</p>
+				<p class="text-black text-opacity-40">Test subject</p>
 				<p class="bg-white text-black flex items-center">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae enim ut mi
 					lobortis commodo vitae ut justo. Etiam rutrum ligula justo, sit amet elementum sapien
@@ -56,7 +56,7 @@
 		</div>
 		<div class="hStack">
 			<div class="w-10/12 bg-white p-2 stack gap-2 rounded-md">
-				<p class="text-black text-opacity-20">Test subject</p>
+				<p class="text-black text-opacity-40">Test subject</p>
 				<p class="bg-white text-black flex items-center">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae enim ut mi
 					lobortis commodo vitae ut justo. Etiam rutrum ligula justo, sit amet elementum sapien
