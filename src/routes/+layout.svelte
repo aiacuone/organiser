6ae0b28ceaac5945a91f6c52836c16ea0b237351
aiacuone <age.iacuone@aventus.io>
2023-08-01 22:30:41 +0100

    button styling header and footer text area and on change listener

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,26 +9,30 @@
 	const space = derived(page, ($page) =>
 		data.spaces.find((space) => space.name === $page.params.space.replace('-', ' '))
 	);
-	$: $space, console.log($space);
 </script>
 
 <div class="stack h-screen text-white">
-	<header class="bg-neutral-600 center">
+	<header class="bg-neutral-600 center py-2">
 		<div class="hStack gap-4">
 			{#each spaces as space}
 				{@const spaceName = space.name.replace(' ', '-')}
-				<a href="/{spaceName}/today" class="capitalize">{space.name}</a>
+				<a href="/{spaceName}/today" class="capitalize bg-neutral-500 px-3 rounded-md"
+					>{space.name}</a
+				>
 			{/each}
 		</div>
 	</header>
 	<div class="bg-neutral-300 flex-1">
 		<slot />
 	</div>
-	<footer class="bg-neutral-600">
+	<footer class="bg-neutral-600 py-2">
 		<div class="hStack center capitalize gap-4">
 			{#each options as time}
 				{@const timeName = time.name.replace(' ', '-')}
-				<a href="/{$space.name.replace(' ', '-')}/{timeName}" class="capitalize">{time.name}</a>
+				<a
+					href="/{$space.name.replace(' ', '-')}/{timeName}"
+					class="capitalize bg-neutral-500 px-3 rounded-md">{time.name}</a
+				>
 			{/each}
 		</div>
 	</footer>
--- src/routes/[space]/[time]/+page.svelte
+++ src/routes/[space]/[time]/+page.svelte
@@ -4,9 +4,17 @@
 	export let data: SpaceData_int;
 </script>
 
-<div>
+<div class="w-full stack gap-3">
 	<div class="center">
 		<p class="capitalize">{data.space}</p>
 		<p class="capitalize">: {data.time}</p>
+	</div>
+	<div class="w-full">
+		<div
+			class="w-9/12 outline-0 bg-white text-black p-2 rounded-md input"
+			role="textbox"
+			contenteditable
+			on:input={(e) => console.log(e.target.innerText)}
+		/>
 	</div>
 </div>
