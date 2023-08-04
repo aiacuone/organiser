19a5c4f4cb39e009cc2797b618c4ea0d313dc4fc
aiacuone <age.iacuone@aventus.io>
2023-08-05 00:15:23 +0100

    removed redundant space page added color to header and footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
-	import { derived } from 'svelte/store';
+	import { derived, type Readable } from 'svelte/store';
 	import '../app.css';
-	import type { SpaceData_int } from '../types/general';
+	import type { SpaceData_int, Space_int } from '../types/general';
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
+	import { onMount, setContext } from 'svelte';
 	import { goto } from '$app/navigation';
 	export let data: SpaceData_int;
 	const { spaces, options } = data;
 
-	const space = derived(page, ($page) =>
+	const space: Readable<Space_int | undefined> = derived(page, ($page) =>
 		$page.params.space
 			? data.spaces.find((space) => space.name === $page.params.space.replace('-', ' '))
 			: spaces[0]
 	);
+	setContext('space', space);
 
 	onMount(() => {
 		const goToDefaultSpace = () => goto(`/${spaces[0].name.replace(' ', '-')}/today`);
@@ -22,12 +23,16 @@
 </script>
 
 <div class="stack h-screen">
-	<header class="bg-neutral-600 center py-2">
+	<header class="center py-2" style="background:{$space?.color}">
 		<div class="hStack gap-4">
-			{#each spaces as space}
-				{@const spaceName = space.name.replace(' ', '-')}
-				<a href="/{spaceName}/today" class="capitalize bg-neutral-500 px-3 rounded-md text-white"
-					>{space.name}</a
+			{#each spaces as _space}
+				{@const spaceName = _space.name.replace(' ', '-')}
+				<a
+					href="/{spaceName}/today"
+					class={`capitalize bg-neutral-500 px-3 rounded-md  ${
+						$space?.name === _space.name ? 'ring-1 ring-opacity-10 ring-black' : 'border-0'
+					}`}
+					style="background:{_space.color}">{_space.name}</a
 				>
 			{/each}
 		</div>
@@ -35,13 +40,13 @@
 	<div class="bg-neutral-100 flex-1">
 		<slot />
 	</div>
-	<footer class="bg-neutral-600 py-2">
+	<footer class=" py-2" style="background:{$space?.color}">
 		<div class="hStack center capitalize gap-4">
 			{#each options as option}
 				{@const timeName = option.name.replace(' ', '-')}
 				<a
 					href="/{$space?.name.replace(' ', '-')}/{timeName}"
-					class="capitalize bg-neutral-500 px-3 rounded-md text-white">{option.name}</a
+					class="capitalize bg-neutral-200 px-3 rounded-md">{option.name}</a
 				>
 			{/each}
 		</div>
--- src/routes/[space]/[time]/+page.svelte
+++ src/routes/[space]/[time]/+page.svelte
@@ -1,52 +1,73 @@
 <script lang="ts">
 	import type { SpaceData_int } from '../../../types/general';
 	import Icon from '@iconify/svelte';
+	import NewNote from '../../../components/Note/New.svelte';
+	import { getContext } from 'svelte';
 
 	interface PageData extends SpaceData_int {
 		time: string;
 	}
 
 	export let data: PageData;
+
+	const space = getContext('space');
 </script>
 
-<div class="w-full stack gap-3">
-	<div class="w-full stack gap-6 text-xs sm:text-sm md:text-md">
-		<div class="flex-col sm:flex-row center gap-2">
-			<div class="w-full sm:w-10/12 bg-white p-2 stack gap-2 rounded-md">
-				<p class="text-opacity-20 text-black">Test subject</p>
-				<p class="bg-white flex items-center">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae enim ut mi
-					lobortis commodo vitae ut justo. Etiam rutrum ligula justo, sit amet elementum sapien
-					ultrices eu. Praesent sit amet nisl volutpat quam fermentum suscipit. Aenean sodales ante
-					sed enim porta, sit amet viverra nulla porta. Curabitur sodales pharetra.
-				</p>
-			</div>
-			<div class="hStack gap-2 center w-2/12">
-				<button class="bg-white bg-opacity-70 rounded-sm p-1"
-					><Icon icon="mdi:pencil" height="25px" /></button
-				>
-				<button class="bg-white bg-opacity-70 rounded-sm p-1"
-					><Icon icon="akar-icons:cross" height="25px" /></button
-				>
-			</div>
+<div class="h-full center stack">
+	<div class="stack gap-4">
+		<div class="center text-xl hStack gap-2">
+			<p class="capitalize text-opacity-40 text-black">{$space.name}</p>
+			<p class="text-opacity-40 text-black">-</p>
+			<p class="capitalize">{data.time}</p>
 		</div>
-		<div class="flex-col sm:flex-row center gap-2">
-			<div class="w-full sm:w-10/12 bg-white p-2 stack gap-2 rounded-md">
-				<p class="text-opacity-20 text-black">Test subject</p>
-				<p class="bg-white flex items-center">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae enim ut mi
-					lobortis commodo vitae ut justo. Etiam rutrum ligula justo, sit amet elementum sapien
-					ultrices eu. Praesent sit amet nisl volutpat quam fermentum suscipit. Aenean sodales ante
-					sed enim porta, sit amet viverra nulla porta. Curabitur sodales pharetra.
-				</p>
-			</div>
-			<div class="hStack gap-2 center w-2/12">
-				<button class="bg-white bg-opacity-70 rounded-sm p-1"
-					><Icon icon="mdi:pencil" height="25px" /></button
-				>
-				<button class="bg-white bg-opacity-70 rounded-sm p-1"
-					><Icon icon="akar-icons:cross" height="25px" /></button
-				>
+		<NewNote background={$space.color} />
+		<div
+			class="w-full center rounded-md py-2 sm:p-4 max-w-screen-lg"
+			style="background:{$space?.color}"
+		>
+			<div class="w-full stack gap-3">
+				<div class="w-full stack gap-6 text-xs sm:text-sm md:text-md">
+					<div class="flex-col sm:flex-row center gap-2">
+						<div class="w-full sm:w-10/12 bg-white p-2 stack gap-2 rounded-md">
+							<p class="text-opacity-20 text-black">Test subject</p>
+							<p class="bg-white flex items-center">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae enim ut mi
+								lobortis commodo vitae ut justo. Etiam rutrum ligula justo, sit amet elementum
+								sapien ultrices eu. Praesent sit amet nisl volutpat quam fermentum suscipit. Aenean
+								sodales ante sed enim porta, sit amet viverra nulla porta. Curabitur sodales
+								pharetra.
+							</p>
+						</div>
+						<div class="hStack gap-2 center w-2/12">
+							<button class="bg-white bg-opacity-70 rounded-sm p-1"
+								><Icon icon="mdi:pencil" height="25px" /></button
+							>
+							<button class="bg-white bg-opacity-70 rounded-sm p-1"
+								><Icon icon="akar-icons:cross" height="25px" /></button
+							>
+						</div>
+					</div>
+					<div class="flex-col sm:flex-row center gap-2">
+						<div class="w-full sm:w-10/12 bg-white p-2 stack gap-2 rounded-md">
+							<p class="text-opacity-20 text-black">Test subject</p>
+							<p class="bg-white flex items-center">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae enim ut mi
+								lobortis commodo vitae ut justo. Etiam rutrum ligula justo, sit amet elementum
+								sapien ultrices eu. Praesent sit amet nisl volutpat quam fermentum suscipit. Aenean
+								sodales ante sed enim porta, sit amet viverra nulla porta. Curabitur sodales
+								pharetra.
+							</p>
+						</div>
+						<div class="hStack gap-2 center w-2/12">
+							<button class="bg-white bg-opacity-70 rounded-sm p-1"
+								><Icon icon="mdi:pencil" height="25px" /></button
+							>
+							<button class="bg-white bg-opacity-70 rounded-sm p-1"
+								><Icon icon="akar-icons:cross" height="25px" /></button
+							>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
