a506765093cda33cb5ec3b33fc48781d6f1093e6
aiacuone <age.iacuone@aventus.io>
2023-08-25 22:59:39 +0100

    hide-scrollbar class added notes container height responsive with height of window

--- src/routes/[space]/[time]/+page.svelte
+++ src/routes/[space]/[time]/+page.svelte
@@ -54,16 +54,21 @@
 
 	let newNoteContentValue: string = '';
 	let newNoteSubjectValue: string = '';
+
+	let headerContainer: number;
+	let parentContainerHeight: number;
+	let notesContainerHeight: number;
+	$: parentContainerHeight, (notesContainerHeight = parentContainerHeight - headerContainer - 30);
 </script>
 
-<div class="h-full center stack">
-	<div class="stack gap-2 w-full px-2 max-w-screen-md">
-		<div class="center text-xl hStack gap-2">
-			<p class="capitalize text-opacity-40 text-black">{$space.name}</p>
-			<p class="text-opacity-40 text-black">-</p>
-			<p class="capitalize">{data.time}</p>
-		</div>
-		<div class="stack gap-6">
+<div class="h-full center stack" bind:clientHeight={parentContainerHeight}>
+	<div class="stack gap-2 w-full px-2 max-w-screen-md h-full sm:h-auto justify-center">
+		<div bind:clientHeight={headerContainer}>
+			<div class="center text-xl hStack gap-2">
+				<p class="capitalize text-opacity-40 text-black">{$space.name}</p>
+				<p class="text-opacity-40 text-black">-</p>
+				<p class="capitalize">{data.time}</p>
+			</div>
 			{#if $page.params.time === 'today' || $page.params.time === 'yesterday'}
 				<NewNote
 					background={$space.color}
@@ -72,7 +77,12 @@
 					bind:subjectValue={newNoteSubjectValue}
 				/>
 			{/if}
-			<div class="w-full center rounded-sm p-2" style="background:{$space?.color}">
+		</div>
+		<div class="stack gap-6">
+			<div
+				class="w-full rounded-sm p-2 overflow-y-scroll hide-scrollbar"
+				style="background:{$space?.color}; max-height:{notesContainerHeight}px"
+			>
 				<div class="w-full stack gap-2 text-xs sm:text-sm md:text-md">
 					{#if $filteredNotes.length === 0}
 						<p class="text-opacity-40 text-black text-center">No notes for this date</p>
