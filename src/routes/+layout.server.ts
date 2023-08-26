ef064d2b8c6e7c9bf248eeaed500cbac101dcf4e
aiacuone <age.iacuone@aventus.io>
2023-08-26 16:30:40 +0100

    .env created with MONGO_URL mongodb installed commented out testData in layour.server.ts using db data

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,23 @@
-import { testData } from '../testData';
+// import { testData } from '../testData';
+import { MongoClient } from 'mongodb';
+import { MONGO_URL } from '$env/static/private';
 
 export const load = async (req: Request, res: Response) => {
+	const client = await MongoClient.connect(MONGO_URL);
+	const db = client.db('time-logger');
+	const collection = db.collection('aiacuone');
+
+	const spacesDocument = await collection.findOne({ name: 'spaces' });
+	const spaces = spacesDocument?.data;
+
+	const timesDocument = await collection.findOne({ name: 'times' });
+	const times = timesDocument?.data;
+
+	// return {
+	// 	...testData
+	// };
 	return {
-		...testData
+		spaces,
+		times
 	};
 };
