94ee39cdee0e96267f7fae3b77ea09af5bda9a94
aiacuone <age.iacuone@aventus.io>
2023-09-16 13:00:09 +0100

    add to axios instance headers

--- src/lib/general/axios.ts
+++ src/lib/general/axios.ts
@@ -4,6 +4,8 @@ import { PUBLIC_BASE_URL } from '$env/static/public';
 export const axios = _axios.create({
 	baseURL: PUBLIC_BASE_URL,
 	headers: {
-		'Content-Type': 'application/json'
+		'Content-Type': 'application/json',
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers': 'Content-Type'
 	}
 });
