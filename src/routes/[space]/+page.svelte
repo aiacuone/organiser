a9f743bbd1a0f83896b465123773556f93bb62a9
aiacuone <age.iacuone@aventus.io>
2023-10-24 09:22:12 +0100

    favicon routing from space page without date

---
+++ src/routes/[space]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import type { SpaceData_int } from '$lib/types';
+	import { getHyphenatedStringFromDate } from '$lib/utils/strings';
+	import { onMount } from 'svelte';
+
+	export let data: SpaceData_int;
+
+	onMount(() => {
+		goto(`/${data.space}/${getHyphenatedStringFromDate(new Date())}`);
+	});
+</script>
