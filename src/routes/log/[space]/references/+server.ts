e7d9d77547d66ec867e6a0f96aac68f1c8850d0d
aiacuone <age.iacuone@aventus.io>
2023-11-04 22:49:30 +0000

    favicon

--- src/routes/log/[space]/references/+server.ts
+++
@@ -1,8 +0,0 @@
-// import { getReferences } from '$lib';
-
-// export const GET = async ({ params }) => {
-// 	const { space } = params;
-// 	const references = await getReferences(space);
-
-// 	return new Response(JSON.stringify(references), { status: 200 });
-// };
