98715efa934db2b8d97a39ae03b2e5429bbc418e
aiacuone <age.iacuone@aventus.io>
2023-12-12 21:19:58 +0000

    cors error

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,10 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			edge: false,
-			split: false
+			split: false,
+			csrf: {
+				checkOrigin: false
+			}
 		})
 	}
 };
