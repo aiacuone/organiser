74578190f1386e2010159e28d6a19d2109de3227
aiacuone <age.iacuone@aventus.io>
2024-03-26 23:17:12 +0000

    [OR-71/object-when-exporting] resolved issue where object was being rendered when exporting

--- src/lib/components/Dialog/ExportDialogLogs.svelte
+++ src/lib/components/Dialog/ExportDialogLogs.svelte
@@ -1,18 +1,26 @@
 <script lang="ts">
-	import { logEnumNames, type Log_enum, type Log_int, LogType_enum } from '$lib/types';
+	import { logEnumNames, type Log_int, LogType_enum, Log_enum } from '$lib/types';
 
 	export let logs: Log_int[];
 	export let logKeyValueFilter: Record<Log_enum, boolean>;
 	export let logKeyValueSortFunction: (a: [Log_enum, boolean], b: [Log_enum, boolean]) => number;
 	export let typeFilter: Record<LogType_enum, boolean>;
 	export let showKeys: boolean;
+
+	$: logFilter = ([key, value]: [Log_enum, any]) => {
+		if (key === Log_enum.listItems) return value.length > 0;
+		if (key === Log_enum.questions)
+			return value.some(
+				({ question, answer }: { question: string; answer: string }) => answer || question
+			);
+		if (key === Log_enum.checkboxItems) return value.some(({ text }: { text: string }) => text);
+		return logKeyValueFilter[key] && value;
+	};
 </script>
 
 {#each logs.filter(({ type }) => typeFilter[type]) as log}
 	<div class="stack gap-2 log-stack p-2">
-		{#each Object.entries(log)
-			.filter(([key, value]) => logKeyValueFilter[key] && value)
-			.sort(logKeyValueSortFunction) as [key, value]}
+		{#each Object.entries(log).filter(logFilter).sort(logKeyValueSortFunction) as [key, value]}
 			{#if key === 'listItems'}
 				<ul>
 					{#each value as listItem}
